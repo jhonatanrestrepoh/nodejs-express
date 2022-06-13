1c320f187013d4af2ccc873acdf3c98ec4718fbe
Jhonatan Restrepo <jhonatan10rpo@gmail.com>
2022-06-13 18:42:10 -0500

    This is a practice with express

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express'); 
+const server = express();
+
+server.get('/', function (req, res) { 
+    
+    res.send("<h1>Hola Mundo con Express y Node</h1>"); 
+    res.end();
+
+});
+
+server.listen(3000, function () { console.log('Server on port 3000');
+
+});
